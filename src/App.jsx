b191a55607fd487df282bbd00be791b7648ae0d5
Sangram Thapa <thapasangram64@gmail.com>
2025-02-09 12:27:06 +0545

    slider added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import axios from "axios";
 import { useEffect } from "react";
-
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 function App() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   async function Get() {
     const response = await axios.get("https://fakestoreapi.com/carts");
     console.log(response);
@@ -13,15 +22,17 @@ function App() {
   return (
     <div>
       <div>
-        <div>
-          <img src="public/images/first.jpg"></img>
-        </div>
-        <div>
-          <img src="public/images/second.jpg"></img>
-        </div>
-        <div>
-          <img src="public/images/third.jpg"></img>
-        </div>
+        <Slider {...settings}>
+          <div>
+            <img src="public/images/first.jpg" className="h-100 w-100"></img>
+          </div>
+          <div>
+            <img src="public/images/second.jpg" className="h-100 w-100"></img>
+          </div>
+          <div>
+            <img src="public/images/third.jpg" className="h-100 w-100"></img>
+          </div>
+        </Slider>
       </div>
     </div>
   );
